3b35959e7670f084a2b3f24c3a0cf69d4d78bacc
deveshaha <devesh_hanumante@outlook.com>
2023-02-21 12:20:51 +0100

    finished first part

--- app/src/main/java/com/example/a13_menu01/MainActivity.java
+++ app/src/main/java/com/example/a13_menu01/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.a13_menu01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     /*
     En este ejemplo vamos a ver como podemos trabajar con un menu de
@@ -53,15 +56,49 @@ public class MainActivity extends AppCompatActivity {
             String texto = "Ha pulsado" + item.getTitle().toString();
             Toast.makeText(this,texto,Toast.LENGTH_SHORT).show();
             return true;
-        }else if(id == R.id.modo_nocturno){
+        } else if(id == R.id.modo_nocturno){
             Toast.makeText(this,"Modo nocturno Pulsado!",Toast.LENGTH_SHORT).show();
             item.setChecked(!item.isChecked());
+            if (item.isChecked()) {
+                Toast.makeText(this, "Modo nocturno activado!", Toast.LENGTH_SHORT).show();
+                //change background color to black
+                View view = this.getWindow().getDecorView();
+                view.setBackgroundColor(Color.BLACK);
+            } else{
+                Toast.makeText(this,"Modo nocturno desactivado!",Toast.LENGTH_SHORT).show();
+                //change background color to white
+                View view = this.getWindow().getDecorView();
+                view.setBackgroundColor(Color.WHITE);
+            }
             return true;
-        }else if(id == R.id.op1){
-            Toast.makeText(this,"Opcion 1 elegida!",Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.share_action){
+            Toast.makeText(this,"Compartir Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if(id == R.id.delete_action){
+            Toast.makeText(this,"Eliminar Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.create_pdf_action) {
+            Toast.makeText(this,"Crear PDF Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.print_action){
+            Toast.makeText(this,"Imprimir Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.send_action){
+            Toast.makeText(this,"Enviar Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.op1){
+            Toast.makeText(this,"Opción 1 Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.op2){
+            Toast.makeText(this,"Opción 2 Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.op3){
+            Toast.makeText(this,"Opción 3 Pulsado!",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.op4){
+            Toast.makeText(this,"Opción 4 Pulsado!",Toast.LENGTH_SHORT).show();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
